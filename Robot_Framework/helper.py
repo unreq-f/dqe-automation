04db3578241030ca8896b7d1979e210063f9d63e
“Yevtushenko <Vladyslav.Yevtushenko@medecision.com>
2026-05-03 19:54:40 +0300

    Robot fixed

--- Robot_Framework/helper.py
+++ Robot_Framework/helper.py
@@ -10,27 +10,17 @@ from selenium.webdriver.chrome.options import Options
 
 #functions from previous module
 #read html table
-def read_html_table_to_dataframe(html_path, driver_path):
-    service = Service(executable_path=driver_path)
-    options = Options()
-    driver = webdriver.Chrome(service=service, options=options)
-    try:
-        driver.get(html_path)
-        driver.set_window_size(1920, 1080)
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "table")))
-        data = {}
-        table = driver.find_element(By.CLASS_NAME, "table")
-        columns = table.find_elements(By.CLASS_NAME, "y-column")
-        for item in columns:
-            by_xpath = item.find_element(By.XPATH, './/*[@id="header"]')
-            row_element = item.find_element(By.ID, "cells1")
-            rows = item.find_elements(By.CLASS_NAME, "column-cell")
-            lst = [row.text for row in rows if row.text != by_xpath.text]
-            data[by_xpath.text] = lst
-        df = pd.DataFrame(data)
-        return df
-    finally:
-        driver.quit()
+def read_html_table_to_dataframe(table):
+    data = {}
+    columns = table.find_elements(By.CLASS_NAME, "y-column")
+    for item in columns:
+        by_xpath = item.find_element(By.XPATH, './/*[@id="header"]')
+        rows = item.find_elements(By.CLASS_NAME, "column-cell")
+        lst = [row.text for row in rows if row.text != by_xpath.text]
+        data[by_xpath.text] = lst
+    df = pd.DataFrame(data)
+    return df
+
 
 #read local data
 def read_partitioned_parquet(base_path, table_name):
@@ -45,17 +35,31 @@ def filter_dataframe(df, column, value):
 
 
 # find diff
-def compare_dataframes_and_return_diff(df1, df2):
-    try:
-        pd.testing.assert_frame_equal(df1, df2)
-        return None
-    except AssertionError as e:
-        diff = df1.compare(df2)
-        return diff
+def check_missing_rows(source: pd.DataFrame, target: pd.DataFrame, key_columns=None):
+    """
+Checks that all rows from the source DataFrame are present in the target DataFrame.
+If rows are missing, it outputs them.
+:param source: source DataFrame
+:param target: target DataFrame
+:param key_columns: list of columns to compare (if None, compares all columns)
+    """
+    if key_columns is None:
+        mask = ~source.apply(tuple, 1).isin(target.apply(tuple, 1))
+        missing = source[mask]
+    else:
+        merged = pd.merge(source[key_columns], target[key_columns], how='left', indicator=True)
+        missing = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+    assert missing.empty, f"Missing rows in target:\n{missing}"
+
+
 
 def read_single_partition(base_path, table_name, partition_value):
     partition_path = pathlib.Path(base_path) / table_name / f"partition_date={partition_value}"
     if not partition_path.exists():
         raise FileNotFoundError(f"path {partition_path} not found")
     df = pd.read_parquet(partition_path, engine='pyarrow')
+    return df
+
+def normalize_columns(df):
+    df.columns = [col.strip().lower().replace(' ', '_') for col in df.columns]
     return df
--- Robot_Framework/test.robot
+++ Robot_Framework/test.robot
@@ -8,44 +8,22 @@ Library    Collections
 ${REPORT_FILE}      report.html
 ${DRIVER_PATH}      chromedriver-win64/chromedriver.exe
 ${PARQUET_FOLDER}   parquet_data
-@{FILTER_DATE}      2026-03-19    2026-03-18     2026-03-17      2026-03-16
+${FILTER_DATE}      2026-03-16
 ${DATE_COLUMN}      visit_date
 ${TABLE_NAME}       facility_type_avg_time_spent_per_visit_date
 
 *** Test Cases ***
-Read_HTML_debug
+Diff_Report
     ${html_path}=    Evaluate    __import__('os').path.abspath(r'''${REPORT_FILE}''')
-    Log     ${html_path}
-    ${driver_path}=    Evaluate    __import__('os').path.abspath(r'''${DRIVER_PATH}''')
-    Log     ${driver_path}
-    ${df1}=     Read Html Table To Dataframe   ${html_path}    ${driver_path}
-    Log         ${df1}
-
-Read_parquet_debug
-    FOR  ${filter_date}     IN      @{FILTER_DATE}
-        ${abs_path1}=    Evaluate    __import__('os').path.abspath(r'''${PARQUET_FOLDER}''')
-        ${df}=    Read Partitioned Parquet    ${PARQUET_FOLDER}    ${TABLE_NAME}
-        Log     ${df}
-        ${filtered_df}=     Filter Dataframe    ${df}   ${DATE_COLUMN}      ${filter_date}
-        Log     ${filtered_df}
-    END
-
-Compare_df
-    ${html_path}=    Evaluate    __import__('os').path.abspath(r'''${REPORT_FILE}''')
-    Log     ${html_path}
-    ${driver_path}=    Evaluate    __import__('os').path.abspath(r'''${DRIVER_PATH}''')
-    Log     ${driver_path}
-    ${df_html}=     Read Html Table To Dataframe   ${html_path}    ${driver_path}
-    Log         ${df_html}
+    Open Browser        ${html_path}        chrome
+    ${table}=    Get WebElement    class=table
+    ${html_df}=    Read Html Table To Dataframe    ${table}
+    Log    ${html_df}
     ${parquet_path}=    Evaluate    __import__('os').path.abspath(r'''${PARQUET_FOLDER}''')
     ${parquet_data}=    Read Partitioned Parquet    ${PARQUET_FOLDER}    ${TABLE_NAME}
-    Log     ${parquet_data}
-    FOR  ${filter_date}     IN      @{FILTER_DATE}
-            ${filtered_parquet}=     Filter Dataframe    ${parquet_data}   ${DATE_COLUMN}      ${filter_date}
-            ${filtered_HTML}=     Filter Dataframe    ${parquet_data}   ${DATE_COLUMN}      ${filter_date}
-            Log     ${filtered_parquet}
-            Log     ${filtered_HTML}
-            Compare Dataframes And Return Diff      ${filtered_parquet}         ${filtered_HTML}
-    END
-
-
+    ${filtered_parquet}=     Filter Dataframe    ${parquet_data}   ${DATE_COLUMN}      ${FILTER_DATE}
+    ${filtered_HTML}=     Filter Dataframe    ${html_df}   Visit Date      ${FILTER_DATE}
+    Log    ${filtered_parquet}
+    Log    ${filtered_HTML}
+    Check Missing Rows      ${filtered_parquet}     ${filtered_HTML}
+    [Teardown]    Close Browser
